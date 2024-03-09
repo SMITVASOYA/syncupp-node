4181dec3c255518e2ca0e3e91089da232dd269f5
Smit Patel <smitvtridhyatech@gmail.com>
2024-03-09 22:51:42 +0530

    added the timeout in the api call

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -43,7 +43,7 @@ class PaymentService {
           `${process.env.RAZORPAY_KEY_ID}:${process.env.RAZORPAY_SECRET}`
         ).toString("base64")}`,
       },
-      timeout: 2500,
+      timeout: 10000,
     });
   }
 
