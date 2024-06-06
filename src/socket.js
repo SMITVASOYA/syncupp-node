8809571d6ce357fc7228aa867386c1f311eabf53
Smit Vasoya <smitvtridhyatech@gmail.com>
2024-06-06 10:57:32 +0530

    fix: removed user_type from the socket events

--- src/socket.js
+++ src/socket.js
@@ -145,7 +145,6 @@ exports.socket_connection = (http_server) => {
             from_user,
             data_reference_id: new_chat?._id,
             message,
-            user_type,
             workspace_id,
           });
 
@@ -304,7 +303,7 @@ exports.socket_connection = (http_server) => {
     socket.on("IMAGES", async (payload) => {
       try {
         console.log("IMAGES");
-        const { from_user, to_user, buffer, user_type, ext } = payload;
+        const { from_user, to_user, buffer, ext } = payload;
 
         const configuration = await Configuration.findOne().lean();
         // removed the size validaions
@@ -349,7 +348,6 @@ exports.socket_connection = (http_server) => {
             message_type: new_message?.message_type,
             _id: new_message?._id,
             createdAt: new_message?.createdAt,
-            user_type,
           });
 
           socket.to(to_user).emit("RECEIVED_IMAGE", {
@@ -359,7 +357,6 @@ exports.socket_connection = (http_server) => {
             message_type: new_message?.message_type,
             _id: new_message?._id,
             createdAt: new_message?.createdAt,
-            user_type,
           });
         }
       } catch (error) {
@@ -373,7 +370,7 @@ exports.socket_connection = (http_server) => {
     socket.on("DOCUMENTS", async (payload) => {
       try {
         console.log("DOCUMENTS");
-        const { from_user, to_user, buffer, user_type, ext } = payload;
+        const { from_user, to_user, buffer, ext } = payload;
 
         const configuration = await Configuration.findOne().lean();
 
@@ -422,7 +419,6 @@ exports.socket_connection = (http_server) => {
             "RECEIVED_DOCUMENT",
             {
               document_url: document_name,
-              user_type,
               from_user,
               to_user,
               createdAt: new_message?.createdAt,
